c2a781c47f058d343997eda6331547093646abad
Yuliia Khalina <Yuliia_Khalina@epam.com>
2019-08-05 14:14:27 +0300

    added redux

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,10 @@ class App extends Component {
     state = { images: [] };
     onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term},
+            params: {
+                query: term,
+                per_page: 20
+            },
             headers: {
                 Authorization: 'Client-ID 5110e0875d03049c42ef2483cf9a9ad53c6a0f46dd526e9ee18dca0c3c6a8f0b'
             }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.css'
-
+import {createStore} from "redux";
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const image = (state = []) => state;
+const store = createStore(image);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
