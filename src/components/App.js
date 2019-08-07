278a801c0db67a690aea2e808112e9383a86fcf8
JuliaKH <43015141+JuliaKH@users.noreply.github.com>
2019-08-07 15:37:44 +0300

    added promise

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react'
 import Header from './Header/Header'
 import axios from "axios";
 import ImageList from './ImageList/ImageList';
+import Unsplash from 'unsplash-js';
+
+const unsplash = new Unsplash({
+    applicationId: "5110e0875d03049c42ef2483cf9a9ad53c6a0f46dd526e9ee18dca0c3c6a8f0b",
+    secret: "4d06872ad12bef5338ff8e64b9324d488905a04733fbe6e76ca5901ff889f8cf"
+});
 
 class App extends Component {
     state = { images: [] };
     // go into service
-    onSearchSubmit = async (term) => {
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
+    // onSearchSubmit = async (term) => {
+    //     const response = await axios.get('https://api.unsplash.com/search/photos', {
+    //         params: {
+    //             query: term,
+    //             per_page: 20
+    //         },
+    //         headers: {
+    //             Authorization: 'Client-ID 5110e0875d03049c42ef2483cf9a9ad53c6a0f46dd526e9ee18dca0c3c6a8f0b'
+    //         }
+    //     });
+    //     console.log(response);
+    //     this.setState({ images: response.data.results })
+    // };
+
+    onSearchSubmit = (term) => {
+        const options = {
             params: {
                 query: term,
                 per_page: 20
@@ -15,9 +35,17 @@ class App extends Component {
             headers: {
                 Authorization: 'Client-ID 5110e0875d03049c42ef2483cf9a9ad53c6a0f46dd526e9ee18dca0c3c6a8f0b'
             }
-        });
-        // console.log(response);
-        this.setState({ images: response.data.results })
+        };
+        axios.get('https://api.unsplash.com/search/photos', options)
+            .then((response) => {
+                this.setState({
+                    images: response.data.results
+                });
+                console.log(this.state);
+            })
+                .catch((mes) => {
+                   console.log(mes);
+                });
     };
     render() {
         // const {images} = this.props
