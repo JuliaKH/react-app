f9034dee1d0e3beacddfbbf15d5bea2bad10c0fc
Yuliia Khalina <Yuliia_Khalina@epam.com>
2019-08-14 13:00:15 +0300

    added response to ImageCard

--- src/pages/ImageCard/ImageCard.js
+++ src/pages/ImageCard/ImageCard.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
 import './ImageCard.scss'
+import axios from "axios";
 
 class ImageCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {image: {
+            urls: {}
+            }}
+    }
 
+    componentDidMount() {
+        this.getImageById(this.props.match.params.id);
+    }
+
+    getImageById = (id) => {
+        const options = {
+            params: {
+            },
+            headers: {
+                Authorization: 'Client-ID 5110e0875d03049c42ef2483cf9a9ad53c6a0f46dd526e9ee18dca0c3c6a8f0b'
+            }
+        };
+        axios
+            .get(`https://api.unsplash.com/photos/${id}`, options)
+            .then(res => {
+                this.setState({image: res.data});
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
     render() {
+        console.log(this.state.image.urls);
         return (
             <section className="card">
-                <img src={`https://source.unsplash.com/${this.props.match.params.id}`}
-                     alt={`https://source.unsplash.com/${this.props.match.params.id}`} className="card__image" />
+                <img src={this.state.image.urls.regular}  alt={this.state.image.alt_description} className="card__image" />
             </section>
         );
     }
